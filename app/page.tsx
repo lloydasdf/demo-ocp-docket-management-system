1838961d3771e816b4e410119e74d90382c13fb2
lloydedwin123-fullstack <lloydedwin.123@gmail.com>
2026-05-29 07:54:32 +0800

    Redirect home to cases page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-'use client';
-
-import { Sidebar } from '@/components/sidebar';
-import Dashboard from '@/components/pages/dashboard';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen bg-background">
-      <Sidebar />
-      <main className="flex-1 overflow-auto">
-        <Dashboard />
-      </main>
-    </div>
-  );
+  redirect('/cases');
 }
